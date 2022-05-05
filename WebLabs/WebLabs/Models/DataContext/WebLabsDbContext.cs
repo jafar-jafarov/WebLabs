8021e61c773d12d07bb18f927776b128381c3041
Asus <Asus@DESKTOP-QC7CL3A>
2022-05-06 00:45:54 +0400

    First Real Versionn

---
+++ WebLabs/WebLabs/Models/DataContext/WebLabsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLabs.Models.DataContext
+{
+    public class WebLabsDbContext
+    {
+    }
+}
---
+++ WebLabs/WebLabs/Models/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLabs.Models.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime? DeletedTime { get; set; }
+        public int? CreatedByUserId { get; set; }
+        public int? DeletedByUserId { get; set; }
+    }
+}
